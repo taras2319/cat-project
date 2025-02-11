881cbdaa1c56bae5d266caa971cdf100abe36108
taras2319 <taras2319@gmail.com>
2025-02-11 19:59:05 +0200

    done

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,7 @@ Route::get('/dashboard', function () {
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
+    Route::get('/profile', [ProfileController::class, 'inddddex'])->name('profile');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
